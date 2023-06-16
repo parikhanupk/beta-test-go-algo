591deabba5152ba25f40696f82f6fd8b292c95c2
Anup Parikh <84572003+parikhanupk@users.noreply.github.com>
2023-06-16 14:55:12 +0530

    Create linked_list.go

---
+++ linked_list.go
@@ -0,0 +1,145 @@
+package main
+import (
+    "fmt")
+
+
+type Cell struct {
+    data string
+    next *Cell
+}
+
+
+type LinkedList struct {
+    sentinel *Cell
+}
+
+
+func make_linked_list() LinkedList {
+    list := LinkedList {}
+    list.sentinel = &Cell { "SENTINEL", nil }
+    return list
+}
+
+
+// Add a cell after me.
+func (me *Cell) add_after(after *Cell) {
+    after.next = me.next
+    me.next = after
+}
+
+
+// Delete a cell after me.
+func (me *Cell) delete_after() *Cell {
+    deleted := me.next
+    if deleted == nil {
+        panic("Trying to delete a non-existent cell")
+    }
+    me.next = deleted.next
+    return deleted
+}
+
+
+// Append values from an array to the linked list
+func (list *LinkedList)add_range(values []string) {
+    last_cell := list.sentinel
+    for ; last_cell.next != nil; last_cell = last_cell.next {}
+    for i := 0; i < len(values); i++ {
+        value_cell := &Cell { values[i], nil }
+        last_cell.add_after(value_cell)
+        last_cell = value_cell
+    }
+}
+
+
+func (list *LinkedList) to_string(separator string) string {
+    str := ""
+    cell := list.sentinel.next
+    if cell != nil {
+        str = cell.data
+        cell = cell.next
+    }
+    for cell != nil {
+        str += (separator + cell.data)
+        cell = cell.next
+    }
+    return str
+}
+
+
+func (list *LinkedList) length() (count int) {
+    for cell := list.sentinel.next; cell != nil; cell = cell.next {
+        count += 1
+    }
+    return
+}
+
+
+func (list *LinkedList) is_empty() bool {
+    return list.sentinel.next == nil
+}
+
+
+func (list *LinkedList) contains(value string) bool {
+    for cell := list.sentinel.next; cell != nil; cell = cell.next {
+        if cell.data == value {
+            return true
+        }
+    }
+    return false
+}
+
+
+// Returns the cell before the cell containing the value
+// Returns nil if the list doesn't contain the value
+func (list *LinkedList) find(value string) *Cell {
+    for cell := list.sentinel; cell.next != nil; {
+        if cell.next.data == value {
+            return cell
+        }
+        cell = cell.next
+    }
+    return nil
+}
+
+
+func (list *LinkedList) remove(value string) bool {
+    cell := list.find(value)
+    if cell != nil {
+        cell.delete_after()
+        return true
+    }
+    return false
+}
+
+
+func (list *LinkedList) push(value string) {
+    list.sentinel.add_after(&Cell { value, nil })
+}
+
+
+func (list *LinkedList) pop() string {
+    return list.sentinel.delete_after().data
+}
+
+
+func main() {
+    // Make a list from an array of values.
+    greek_letters := []string { "α", "ß", "γ", "δ", "ϵ" }
+    list := make_linked_list()
+    list.add_range(greek_letters)
+    fmt.Println(list.to_string(" "))
+    fmt.Println()
+
+    // Demonstrate a stack.
+    stack := make_linked_list()
+    stack.push("Apple")
+    stack.push("Banana")
+    stack.push("Coconut")
+    stack.push("Date")
+    for !stack.is_empty() {
+        fmt.Printf("Popped: %-7s   Remaining %d: %s\n",
+            stack.pop(),
+            stack.length(),
+            stack.to_string(" "))
+    }
+}
